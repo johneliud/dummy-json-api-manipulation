ded3e2c4e74e73408d76306e9560a8065d939efe
johnodhiambo0 <johneliud4@gmail.com>
2024-12-20 09:48:14 +0300

    add: data structures representing the type of API structure to be used when fetching and storing information.

---
+++ recipes/models/models.go
@@ -0,0 +1,24 @@
+package models
+
+type Recipes struct {
+	Recipes []Recipe
+}
+
+type Recipe struct {
+	ID                 int      `json:"id"`
+	Name               string   `json:"name"`
+	Ingredients        []string `json:"ingredients"`
+	Instructions       []string `json:"instructions"`
+	PrepTimeMinutes    int      `json:"prepTimeMinutes"`
+	CookTimeMinutes    int      `json:"cookTimeMinutes"`
+	Servings           int      `json:"servings"`
+	Difficulty         string   `json:"difficulty"`
+	Cuisine            string   `json:"cuisine"`
+	CaloriesPerServing int      `json:"caloriesPerServing"`
+	Tags               []string `json:"tags"`
+	UserID             int      `json:"userId"`
+	Image              string   `json:"image"`
+	Rating             float64  `json:"rating"`
+	ReviewCount        int      `json:"reviewCount"`
+	MealType           []string `json:"mealType"`
+}
