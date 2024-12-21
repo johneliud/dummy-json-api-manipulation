bd68d0068acf7f806660ce4af54e44fe60e48ccc
John <johneliud4@gmail.com>
2024-12-21 15:40:42 +0300

    add: main function of the program that serves the static folder and establishes a connection for accessing the webpage.

---
+++ recipes/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"recipes/controllers"
+	"recipes/utils"
+)
+
+func main() {
+	port := utils.GetPort()
+
+	http.HandleFunc("/", controllers.IndexHandler)
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	fmt.Printf("Server started at http://localhost%v\n", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		fmt.Printf("Failed to start server: %v\n", err)
+	}
+}
