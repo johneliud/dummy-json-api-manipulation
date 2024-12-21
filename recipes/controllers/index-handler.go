e89c7cdaf77b2977fd7ec336352c82029a6b31ae
John <johneliud4@gmail.com>
2024-12-21 17:38:46 +0300

    update: IndexHandler function by checking the URL path before invoking FetchRecipes function.

--- recipes/controllers/index-handler.go
+++ recipes/controllers/index-handler.go
@@ -7,9 +7,28 @@ import (
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		notFoundHandler(w, r)
+		log.Println("Page Not Found")
+		return
+	}
+
+	recipes, err := utils.FetchRecipes()
+	if err != nil {
+		internalServerErrorHandler(w, r)
+		log.Printf("Failed fetching recipes: %v\n", err)
+		return
+	}
+
+	if len(recipes.Recipes) == 0 {
+		internalServerErrorHandler(w, r)
+		log.Printf("No available recipes. Fetched %d recipes\n", len(recipes.Recipes))
+		return
+	}
+
 	tmpl := utils.ParseTemplates()
 
-	if err := tmpl.ExecuteTemplate(w, "index.html", nil); err != nil {
+	if err := tmpl.ExecuteTemplate(w, "index.html", recipes); err != nil {
 		internalServerErrorHandler(w, r)
 		log.Printf("Error executing template: %v\n", err)
 	}
---
+++ recipes/utils/render-rating-stars.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"html/template"
+	"strings"
+)
+
+// RenderStars dynamically generates star icons based on the rating
+func RenderRatingStars(rating float64) template.HTML {
+	var stars strings.Builder
+	fullStars := int(rating)
+	halfStar := rating-float64(fullStars) >= 0.5
+
+	// Full stars
+	for i := 0; i < fullStars; i++ {
+		stars.WriteString(`<ion-icon name="star"></ion-icon>`)
+	}
+
+	// Half stars
+	if halfStar {
+		stars.WriteString(`<ion-icon name="star-half"></ion-icon>`)
+	}
+
+	// Empty stars
+	for i := fullStars; i < 5; i++ {
+		if halfStar && i == fullStars {
+			continue
+		}
+		stars.WriteString(`<ion-icon name="star-outline"></ion-icon>`)
+	}
+	return template.HTML(stars.String())
+}
