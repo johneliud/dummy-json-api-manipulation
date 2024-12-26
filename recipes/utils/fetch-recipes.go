2d1694d09fde7ebe79894ea404bf0404be871aff
John <johneliud4@gmail.com>
2024-12-26 13:04:47 +0300

    add: FetchRecipesByID function that takes an id as a parameter to access recipe for a specified ID.

--- recipes/utils/fetch-recipes.go
+++ recipes/utils/fetch-recipes.go
@@ -33,10 +33,42 @@ func FetchRecipes() (models.Recipes, error) {
 		return models.Recipes{}, fmt.Errorf("error reading response body: %v", err)
 	}
 
-	var Recipes models.Recipes
-	err = json.Unmarshal(body, &Recipes)
+	var recipes models.Recipes
+	err = json.Unmarshal(body, &recipes)
 	if err != nil {
 		return models.Recipes{}, fmt.Errorf("error unmarshaling JSON: %v", err)
 	}
-	return Recipes, nil
+	return recipes, nil
+}
+
+func FetchRecipeByID(id int) (models.Recipe, error) {
+	url := fmt.Sprintf("https://dummyjson.com/recipes/%d", id)
+
+	client := http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return models.Recipe{}, fmt.Errorf("error creating request: %v", err)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return models.Recipe{}, fmt.Errorf("error making HTTP request: %v", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return models.Recipe{}, fmt.Errorf("received status code %d", res.StatusCode)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return models.Recipe{}, fmt.Errorf("error reading response body: %v", err)
+	}
+
+	var recipe models.Recipe
+	err = json.Unmarshal(body, &recipe)
+	if err != nil {
+		return models.Recipe{}, fmt.Errorf("error unmarshaling JSON: %v", err)
+	}
+	return recipe, nil
 }
