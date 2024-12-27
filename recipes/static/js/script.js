9a6ba729afd7a841034660192fc9a83b49d75ca6
John <johneliud4@gmail.com>
2024-12-27 11:02:53 +0300

    add: renderRatingStars function used to generate stars based on the rating score.

--- recipes/static/js/script.js
+++ recipes/static/js/script.js
@@ -6,3 +6,25 @@ links.forEach((li) => {
     li.classList.add("active");
   });
 });
+
+let recipes = [];
+
+const renderRatingStars = (rating) => {
+  const fullStars = Math.floor(rating);
+  const halfStar = rating % 1 >= 0.5;
+  let stars = "";
+
+  for (let i = 0; i < fullStars; i++) {
+    stars += `<ion-icon name="star"></ion-icon>`;
+  }
+
+  if (halfStar) {
+    stars += `<ion-icon name="star-half"></ion-icon>`;
+  }
+
+  const emptyStars = 5 - Math.ceil(rating);
+  for (let i = 0; i < emptyStars; i++) {
+    stars += '<ion-icon name="star-outline"></ion-icon>';
+  }
+  return stars;
+};
