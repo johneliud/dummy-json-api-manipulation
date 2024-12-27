90fe2bf5bf7752c316eddf4dff0ebcd14ca9a9b4
John <johneliud4@gmail.com>
2024-12-27 10:24:16 +0300

    add: struct element representing featured recipe details.

--- recipes/controllers/index-handler.go
+++ recipes/controllers/index-handler.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"log"
 	"net/http"
+	"recipes/models"
 	"recipes/utils"
 )
 
@@ -26,9 +27,17 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	data := struct {
+		Recipes []models.Recipe
+		Featured models.Recipe
+	}{
+		Recipes: recipes.Recipes,
+		Featured: recipes.Recipes[0], // Default featured
+	}
+
 	tmpl := utils.ParseTemplates()
 
-	if err := tmpl.ExecuteTemplate(w, "index.html", recipes); err != nil {
+	if err := tmpl.ExecuteTemplate(w, "index.html", data); err != nil {
 		internalServerErrorHandler(w, r)
 		log.Printf("Error executing template: %v\n", err)
 		return
